e165dfcbe8b8eb5bcc41e0826bd151053374b2c6
Alex  Fogazzi <alexrfogazzi@gmail.com>
2022-08-31 20:27:28 -0300

    feat: componente Informacoes pessoais e layout home

--- front-end/src/components/HomeContainer.vue
+++ front-end/src/components/HomeContainer.vue
@@ -1,13 +1,14 @@
 <template>
   <div class="home_container">
     <v-parallax
-      id="container_paralax"
-      src="../assets/parallax.png"      
+      class="container_paralax"
+      src="../assets/parallax.png"  
       dark
     >    
       <v-row
         align-item="center"
         justify="center"
+        class="d-flex ma-5"
         >
         <v-col
           class="text-center"
@@ -28,59 +29,50 @@
              Si num tem leite então bota uma pinga aí cumpadi!Quem num gosta di mé, boa gentis num é.
              Manduma pindureta quium dia nois paga.Cevadis im ampola pa arma uma pindureta.
 
-          </p>
-          
+          </p>          
         </v-col>
       </v-row>
     </v-parallax>
 
-    <!-- SUB SEÇÃO MAIN     -->
-    <div id="sub_secao_main">
+    <div id="divisor_container_main">
       <h1 id="texto_subsecao_main">
-        Outro texto de algo
+        "Faça o teu melhor, na condição que você tem, enquanto você não tem condições melhores 
+         para fazer melhor ainda!"
       </h1>
+      <h3><i>Mario Sérgio Cortella</i></h3>
     </div>
+    
+    <!-- SUB SEÇÃO MAIN     -->
     <v-parallax
-      id="container_paralax"
+      class="container_paralax"
       src="../assets/parallax.png"      
       dark
     >    
       <v-row
+        id="row_container_info_pessoal"
         align-item="center"
-        justify="center"
+        justify="space-between"
+        class="d-flex ma-10"        
         >
-        <v-col cols="2">
-          <h1>Mais alguma coisa qualquer</h1>
-        </v-col>
+        <v-col
+          class="d-flex text-center"
+          cols="3"          
+          >
+          <v-img id="imagem_info_pessoal" src="../assets/foto_pessoal.png"></v-img>
+        </v-col>       
         <v-col
           class="text-center"
-          cols="10"
-          
+          cols="8"          
           >
-          <h1 id="titulo_apresentacao" class="mb-6 mt-10">
-            Um breve texto
-          </h1>
-          <h3 class="mb-6 mt-10">
-            Vuetify
-          </h3>
-          <p>
-            Mussum Ipsum, cacilds vidis litro abertis. Mauris nec dolor in eros commodo tempor.
-             Aenean aliquam molestie leo, vitae iaculis nisl.In elementis mé pra quem é amistosis 
-             quis leo.Praesent malesuada urna nisi, quis volutpat erat hendrerit non. Nam vulputate 
-             dapibus.Quem num gosta di mé, boa gentis num é. 
-             Si num tem leite então bota uma pinga aí cumpadi!Quem num gosta di mé, boa gentis num é.
-             Manduma pindureta quium dia nois paga.Cevadis im ampola pa arma uma pindureta.
-
-          </p>
-          
+          <InfoPessoal />          
         </v-col>
       </v-row>
     </v-parallax>
-
   </div>
 </template>
 
 <script>
+import InfoPessoal from "./InfoPessoal.vue";
 
 export default {
     name: "HomeContainer",
@@ -88,7 +80,7 @@ export default {
 
     }),
     
-    components: {  }
+    components: { InfoPessoal }
 }
 </script>
 
@@ -111,19 +103,32 @@ export default {
     border-radius: 50%;
   }
 
-  #sub_secao_main{
+  #divisor_container_main{
     display: flex;
+    flex-direction: column;
     justify-content: center;
     align-items: center;
     height: 40vh;
     background-image: radial-gradient(circle, rgb(46, 44, 44), rgb(121, 121, 113), rgb(47, 49, 47));
     box-shadow: inset 2px 2px 2em rgb(121, 87, 67);
   }
+  #divisor_container_main h3{
+    color: #fff;
+  }
   
   #texto_subsecao_main{
     color: #fff;
     /* text-shadow: 0 0 3px #FF0000; */
     text-shadow: 1px 1px 5px #161894;
+    padding: 0 50px;
+  }
+
+  #row_container_info_pessoal{
+    height: 15%;
+  }
+
+  #imagem_info_pessoal{
+    align-self: end;
   }
 
 
---
+++ front-end/src/components/InfoPessoal.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="info_pessoal">
+    <v-card id="card_info_pessoal">
+      <v-card-title id="titulo_apresentacao">Texto de Informações Pessoais</v-card-title>
+      <v-card-subtitle class="my-2">
+        Aqui falar de formação e experiência profissional
+      </v-card-subtitle>
+      <v-card-text id="texto_info_pessoal">
+        Mussum Ipsum, cacilds vidis litro abertis. Mauris nec dolor in eros commodo tempor.
+        Aenean aliquam molestie leo, vitae iaculis nisl.In elementis mé pra quem é amistosis 
+        quis leo.Praesent malesuada urna nisi, quis volutpat erat hendrerit non. Nam vulputate 
+        dapibus.Quem num gosta di mé, boa gentis num é. 
+        Si num tem leite então bota uma pinga aí cumpadi!Quem num gosta di mé, boa gentis num é.
+        Manduma pindureta quium dia nois paga.Cevadis im ampola pa arma uma pindureta.
+
+      </v-card-text>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'InfoPessoal',
+}
+</script>
+
+<style scoped>
+
+  #card_info_pessoal{
+    background-color: rgba(222, 227, 238, 0.2);
+    border: none;
+    box-shadow: none;
+  }
+
+  #texto_info_pessoal{
+    color: #000;
+  }
+  
+
+</style>
